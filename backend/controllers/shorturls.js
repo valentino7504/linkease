37e90fd77464e699183eff0f55bae147e88dd411
Edwin Ade <edwinmbonyjr@gmail.com>
2024-09-05 08:57:52 +0100

    feat(controller): add controller for /api/v1/shorturls routing

---
+++ backend/controllers/shorturls.js
@@ -0,0 +1,29 @@
+const shorturlRouter = require('express').Router();
+const ShortUrl = require('../models/shorturl');
+
+shorturlRouter.get('/', (req, res) => {
+  ShortUrl.find({})
+    .then(shorturls => res.json(shorturls));
+});
+
+shorturlRouter.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  ShortUrl.findById(id)
+    .then(shorturl => res.json(shorturl))
+    .catch(err => next(err));
+});
+
+shorturlRouter.post('/', (req, res, next) => {
+  const body = req.body;
+
+  const shortUrl = new ShortUrl({
+    fullUrl: body.fullUrl,
+    shortId: body.shortId
+  });
+  shortUrl.save()
+    .then(url => res.json(url))
+    .catch(err => next(err));
+});
+
+module.exports = shorturlRouter;
