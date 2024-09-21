c536dd9250001dca9cfbe2a7cdd20265195157ab
Edwin Ade <edwinmbonyjr@gmail.com>
2024-09-21 16:08:22 +0100

    chore: remove redundant api folder

--- api/index.js
+++
@@ -1,7 +0,0 @@
-const { createServer } = require('http');
-const app = require('../backend/app');
-const server = createServer(app);
-
-module.exports = (req, res) => {
-  server.emit('request', req, res);
-};
