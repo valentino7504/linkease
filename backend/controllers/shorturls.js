915b7eab0c63728207ffcce4ce4a9f0b24d0bc5f
Edwin Ade <edwinmbonyjr@gmail.com>
2024-09-21 12:55:53 +0100

    fix(controllers): update shorturl route to handle duplicate shortId

--- backend/controllers/shorturls.js
+++ backend/controllers/shorturls.js
@@ -1,5 +1,11 @@
 const shorturlRouter = require('express').Router();
 const ShortUrl = require('../models/shorturl');
+const customAlphabet = require('nanoid').customAlphabet;
+const config = require('../utils/config');
+
+const ALLOWED_CHARS = config.ALLOWED_CHARS;
+const URL_LEN = parseInt(config.URL_LENGTH) || 8;
+const urlGen = customAlphabet(ALLOWED_CHARS, URL_LEN);
 
 shorturlRouter.get('/', (req, res) => {
   ShortUrl.find({})
@@ -16,22 +22,32 @@ shorturlRouter.get('/:shortId', (req, res, next) => {
 
 shorturlRouter.post('/', (req, res, next) => {
   const body = req.body;
+  const maxRetries = 5;
+  let attempts = 0;
 
-  ShortUrl.findOne({ fullUrl: body.fullUrl })
-    .then(shorturl => {
-      if (shorturl)
-        return res.json(shorturl);
-      else {
-        const shortUrl = new ShortUrl({
-          fullUrl: body.fullUrl,
-          shortId: body.shortId
-        });
-        shortUrl.save()
-          .then(shorturl => res.json(shorturl))
-          .catch(err => next(err));
-      }
-    })
-    .catch(err => next(err));
+  const trySave = (shortId) => {
+    const shortUrl = new ShortUrl({
+      fullUrl: body.fullUrl,
+      shortId: shortId
+    });
+    shortUrl.save()
+      .then(shorturl => res.json(shorturl))
+      .catch(err => {
+        if (err.code === 11000) {
+          if (shortId)
+            return res.status(409).json({ error: 'Custom short ID is already in use' });
+          else if (attempts < maxRetries) {
+            attempts++;
+            trySave(urlGen());
+          } else {
+            return res.status(500).json({ error: 'Failed to generate a unique short ID after many tries' });
+          }
+        } else {
+          next(err);
+        }
+      });
+  };
+  trySave(body.shortId);
 });
 
 module.exports = shorturlRouter;
